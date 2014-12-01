1556b085e6a00ddcc3e889c21f56da1394706da9
Ophir LOJKINE <pere.jobs@gmail.com>
2014-12-01 02:07:25 +0100

    Add data for hebrew and russian
    A pronouniciation convention close to the one of french has been choosen

---
+++ anylang-fr-ru.js
@@ -0,0 +1,44 @@
+(function anylang_fr_ru() {
+  Anylang.addLang(
+    {
+      "langOrigin" : "fr",
+      "langDestination": "ru",
+      "table" : [
+        {"from":"chtch","to":"щ"},
+        {"from":"tch","to":"ч"},
+        {"from":"ch","to":"ш"},
+        {"from":"ts","to":"ц"},
+        {"from":"iu","to":"ю"},
+        {"from":"ia","to":"я"},
+        {"from":"io","to":"ё"},
+        {"from":"x","to":"х"},
+        {"from":"a","to":"а"},
+        {"from":"b","to":"б"},
+        {"from":"v","to":"в"},
+        {"from":"g","to":"г"},
+        {"from":"d","to":"д"},
+        {"from":"e","to":"е"},
+        {"from":"j","to":"ж"},
+        {"from":"z","to":"з"},
+        {"from":"i","to":"и"},
+        {"from":"j","to":"й"},
+        {"from":"k","to":"к"},
+        {"from":"l","to":"л"},
+        {"from":"m","to":"м"},
+        {"from":"n","to":"н"},
+        {"from":"o","to":"о"},
+        {"from":"p","to":"п"},
+        {"from":"r","to":"р"},
+        {"from":"s","to":"с"},
+        {"from":"t","to":"т"},
+        {"from":"u","to":"у"},
+        {"from":"f","to":"ф"},
+        {"from":'"',"to":"ъ"},
+        {"from":"y","to":"ы"},
+        {"from":"'","to":"ь"},
+        {"from":"è","to":"э"},
+        { from: '|'g, to: '' } //Separator to prevent to consecutive letters from being merged
+        ]
+    });
+})();
+
